369cfcbf1fa7cdd2b1cc98873bb0f88ad266bade
bahubali116 <buzzingakash@yahoo.com>
2017-06-30 18:56:58 +0530

    Created by Coding Ground Client

---
+++ New Project-20170630/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class A {
+  int a;
+  public:
+  A() { cout << "inside A default const \n";}
+  A(int a): a(a) { cout << "inside A parameter const a= "<< a<< "\n";}
+};
+
+class B: public A {
+    int b;
+    public:
+    B(int b): A(b), b(b){ cout << "inside b parameterised const b=" <<b <<"\n";}
+};
+
+int main() {
+    
+    B obj(23);
+	// your code goes here
+	return 0;
+}
